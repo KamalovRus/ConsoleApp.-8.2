40c56d71de7cff9079bf2b38683604446d57f9dd
KamalovRus <kamrus97@mail.ru>
2021-11-25 16:02:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp.Задача8.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Task8_2
+{
+    public static void Main(string[] args)
+    {
+        CreateFileWithRandomNumbers("C:/Users/Рустем/source/Для 8.1/С флэшки/Текстовый файл/Фильм/Фотки/1.txt");
+        ReadandSumFileWithRandomNumbers("C:/Users/Рустем/source/Для 8.1/С флэшки/Текстовый файл/Фильм/Фотки/1.txt");
+    }
+    public static void CreateFileWithRandomNumbers(string path)
+    {
+        Random r = new Random();
+        try
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                string[] temp = new string[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    temp[i] = r.Next(1, 101).ToString();
+                }
+                sw.WriteLine(String.Join(" ", temp));
+            }
+
+        }
+        catch (Exception Ex)
+        {
+            Console.WriteLine(Ex.ToString());
+        }
+    }
+    public static void ReadandSumFileWithRandomNumbers(string path)
+    {
+        using (StreamReader sr = File.OpenText(path))
+        {
+            List<int> list = new List<int>();
+            string s = "";
+            s = sr.ReadLine();
+            Console.WriteLine(s);
+            string[] number = s.Split(' ');
+            foreach (var item in number)
+            {
+                list.Add(Convert.ToInt32(item));
+            }
+            Console.WriteLine(list.Sum());
+            Console.ReadKey();
+
+        }
+    }
+}
